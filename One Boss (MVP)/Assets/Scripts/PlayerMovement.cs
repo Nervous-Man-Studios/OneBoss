ca80b01c36f57d7915021f8e74348864dcde6673
epatrone13 <esteban.patrone.michellod@gmail.com>
2021-06-03 21:24:38 -0400

    Basic 2D movement (less gooo)

--- One Boss (MVP)/Assets/Scripts/PlayerMovement.cs
+++ One Boss (MVP)/Assets/Scripts/PlayerMovement.cs
@@ -6,15 +6,39 @@ public class PlayerMovement : MonoBehaviour	{
 	public float movementSpeed;
 	public Rigidbody2D rb;
 
+	public float jumpForce = 20f;
+	public Transform feet;
+	public LayerMask groundLayers;
+
 	float mx;
 
-	private void Update()	{
+	private void Update() {
 		mx = Input.GetAxisRaw("Horizontal");
+
+		if (Input.GetButtonDown("Jump") && IsGrounded()) {
+			Jump();
+		}
 	}
 
-	private void FixedUpdate()	{
+	private void FixedUpdate() {
 		Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
 
 		rb.velocity = movement;
+	}
+
+	void Jump()	{
+		Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+
+		rb.velocity = movement;
+	}
+
+	public bool IsGrounded () {
+		Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+
+		if (groundCheck != null) {
+			return true;
+		}
+
+		return false;
 	}
 }
