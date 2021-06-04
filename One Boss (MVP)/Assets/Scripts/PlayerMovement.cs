56d17f44efe30746cd13737c2cbc0afd6e84ddc8
epatrone13 <esteban.patrone.michellod@gmail.com>
2021-06-04 17:56:10 -0400

    June 4 Progress
    Dev Art Player + basic animations

--- One Boss (MVP)/Assets/Scripts/PlayerMovement.cs
+++ One Boss (MVP)/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour	{
 	public float movementSpeed;
 	public Rigidbody2D rb;
 
+	public Animator anim;
+
 	public float jumpForce = 20f;
 	public Transform feet;
 	public LayerMask groundLayers;
@@ -18,6 +20,20 @@ public class PlayerMovement : MonoBehaviour	{
 		if (Input.GetButtonDown("Jump") && IsGrounded()) {
 			Jump();
 		}
+
+		if (Mathf.Abs(mx) > 0.05f) {
+			anim.SetBool("isRunning", true);
+		} else {
+			anim.SetBool("isRunning", false);
+		}
+
+		if (mx > 0f) {
+			transform.localScale = new Vector3(1f, 1f, 1f);
+		} else if (mx < 0f) {
+			transform.localScale = new Vector3(-1f, 1f, 1f);
+		}
+
+		anim.SetBool("isGrounded", IsGrounded());
 	}
 
 	private void FixedUpdate() {
